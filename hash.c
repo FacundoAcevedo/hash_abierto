ccd972e194b305eb2e24b4a044873688bb6e44b4
FacundoAcevedo <acv2Facundo@gmail.com>
2012-10-29 17:23:23 -0300

    cambie las funciones del hash, para que anden con la lista enlazada, igual flata bocha, me cambio de carrera?

--- hash.c
+++ hash.c
@@ -7,13 +7,12 @@
 
 /*Funciones privadas */
 size_t _fhash(const char*, size_t);
+nodo_hash_t* _nodo_hash_crear(const char *clave, void *dato);
 
 /*Struct de los nodos del hash*/
 struct nodo_hash{
     char *clave;
     void *valor;
-    /*struct nodo* ref;*/
-    struct nodo_hash* ref;
 };
 
 
@@ -22,7 +21,6 @@ struct hash{
     size_t tamanio;
     hash_destruir_dato_t destruir_dato;
     lista_t** tabla;
-    /*vector_t* tabla;*/
 };
 
 hash_t* hash_crear(hash_destruir_dato_t destruir_dato)
@@ -30,7 +28,7 @@ hash_t* hash_crear(hash_destruir_dato_t destruir_dato)
     hash_t* hash = malloc(sizeof(hash_t));
     if (!hash) return NULL;
     /*Reservo el espacio que necesito para los nodos*/
-    if (!(hash->tabla = calloc(TAM_INI, sizeof(nodo_hash_t))))//Quizas sea nodo_hash_t*
+    if (!(hash->tabla = calloc(TAM_INI, sizeof(lista_t*))))//Quizas sea nodo_hash_t*
     {
         free(hash);
         return NULL;
@@ -59,37 +57,35 @@ bool hash_guardar(hash_t *hash, const char *clave, void *dato)
     //Verifico que el vector apunte a algo.
     size_t vect_pos = _fhash(clave, hash->tamanio);
 
-    /*[>Verifico que haya una lista en la posicion del vector<]*/
-    /*if (!((hash->tabla) + vect_pos))*/
-    /*{*/
-        /**(hash->tabla + vect_pos) = lista_crear();*/
-    /*}*/
-
-    /*Genero el nodo*/
-    nodo_hash_t* nodo_nuevo = malloc(sizeof(nodo_hash_t));
-    nodo_nuevo->clave = clave;
-    nodo_nuevo->valor = dato;
-    nodo_nuevo->ref = NULL;
-    
-
-    /*Verifico si hay algo en la pos donde quiero guardar*/
+    /*Verifico que haya una lista en la posicion del vector*/
     if (!((hash->tabla) + vect_pos))
     {
-        /*Changos, parece que el lugar esta vacio*/
-        *((hash->tabla) + vect_pos) = nodo_nuevo;
-        hash->cantidad +=1;
-        return true;
+        *(hash->tabla + vect_pos) = lista_crear();
     }
-    /*Aca hay un nodo, asi que lo inserto en primer lugar, y lo apunto al siguiente*/
-    /*Obtengo la direccion del primer nodo*/
-    nodo_hash_t* nodo_siguiente = *((hash->tabla) + vect_pos);
-    /*Inserto mi nuevo nodo en dicho lugar*/
-    *((hash->tabla) + vect_pos) = nodo_nuevo;
-    /*Apunto "ref"" de mi nuevo nodo a nodo_siguiente*/
-    nodo_nuevo->ref = nodo_siguiente;
-    /*Aumento la cnatidad en un*/
+
+    /*Genero el nodo*/
+    nodo_hash_t *nodo_nuevo = _nodo_hash_crear(clave, dato);
+    if (!nodo_nuevo) return NULL;
+
+
+    /*Verifico si hay algo en la pos donde quiero guardar*/
+   
+    /*Changos, parece que el lugar esta vacio*/
+    /**((hash->tabla) + vect_pos) = nodo_nuevo;*/
+    lista_insertar_primero(*((hash->tabla) + vect_pos), nodo_nuevo);
     hash->cantidad +=1;
     return true;
+   
+    /*[>Aca hay un nodo, asi que lo inserto en primer lugar, y lo apunto al siguiente<]*/
+    /*[>Obtengo la direccion del primer nodo<]*/
+    /*nodo_hash_t* nodo_siguiente = *((hash->tabla) + vect_pos);*/
+    /*[>Inserto mi nuevo nodo en dicho lugar<]*/
+    /**((hash->tabla) + vect_pos) = nodo_nuevo;*/
+    /*[>Apunto "ref"" de mi nuevo nodo a nodo_siguiente<]*/
+    /*nodo_nuevo->ref = nodo_siguiente;*/
+    /*[>Aumento la cnatidad en un<]*/
+    /*hash->cantidad +=1;*/
+    /*return true;*/
 }//hash_guardar
 
 
@@ -106,14 +102,28 @@ bool hash_pertenece(const hash_t *hash, const char *clave)
    
    //Voy a dicha posicion y recorro los nodos
    if (!((hash->tabla) + vect_pos)) return false;
-   nodo_hash_t* nodo = ((hash->tabla) + vect_pos);
-   while (nodo->ref)
-   {
-       if (*(nodo->clave) == *clave) return true;
-       //Avanzo un nodo
-       nodo = nodo->ref;
 
-   }//while
+   //Creo lo que nececito
+   nodo_hash_t* nodo_actual = NULL;
+   lista_t* lista = *((hash->tabla) + vect_pos);
+   lista_iter_t* iter = lista_iter_crear(lista);
+   
+   //Verifico que existan
+   if (!lista || !iter) return NULL;
+   //Recorro la lista buscando la clave
+   do
+   {
+      nodo_actual = lista_iter_ver_actual(iter);
+      //Verifico si esta
+      if ( strcmp( nodo_actual->clave, clave) == 0){
+          free(iter);
+          return true; 
+      }
+      //No esta, entonces avanzo una pocicion
+      lista_iter_avanzar(iter);
+
+   }while(lista_iter_ver_actual(iter));
+   free(iter);
 
    //No la encontre, pucha!
    return false;
@@ -122,71 +132,27 @@ bool hash_pertenece(const hash_t *hash, const char *clave)
 
 void *hash_obtener(const hash_t *hash, const char *clave)
 {
-    //Es casi igual a hash_pertenece
-    
-   //Obtengo la posicion del nodo 
-   size_t vect_pos = _fhash(clave, hash->tamanio);
-   
-   //Voy a dicha posicion y recorro los nodos
-   if (!((hash->tabla) + vect_pos)) return NULL;
-   nodo_hash_t* nodo = ((hash->tabla) + vect_pos);
-   while (nodo->ref)
-   {
-       if (*(nodo->clave) == *clave) return nodo->valor;
-       //Avanzo un nodo
-       nodo = nodo->ref;
-
-   }//while
-
-   //No la encontre, pucha!
-   return NULL;
 }//*hash_obtener
 
 
 void *hash_borrar(hash_t *hash, const char *clave)
 {
-    
-    //Es casi igual a hash_pertenece
-    
-   //Obtengo la posicion del nodo 
-   size_t vect_pos = _fhash(clave, hash->tamanio);
-   
-   //Voy a dicha posicion y recorro los nodos
-   if (!((hash->tabla) + vect_pos)) return NULL;
-   long indice = 0; //long por que anda a saber si hacen pruebas de volumen :P
-   nodo_hash_t* nodo = ((hash->tabla) + vect_pos);
-   nodo_hash_t* nodo_anterior = nodo;
-   while (nodo->ref)
-   {
-       if (*(nodo->clave) == *clave)
-       {
-           void* valor = nodo->valor;
-           if (indice == 0)
-           {
-               //Estoy en el primer lugar
-               *((hash->tabla) + vect_pos) = nodo->ref;
-           }//if
-           nodo_anterior->ref = nodo->ref;
-           free(nodo);
-           hash->cantidad -= 1;           
-           return valor;
-       }
-           
-       //Avanzo un nodo
-       nodo_anterior = nodo;
-       nodo = nodo->ref;
-       indice++;
-
-   }//while
-
-   //No la encontre, pucha!
-   return NULL;
 }//hash_borrar
+
 
 /*--------------PRIVADAS---------------*/
 
+//Funcion nodo_hash_crear (PRIVADA)
+nodo_hash_t* _nodo_hash_crear(const char *clave, void *dato){
+    nodo_hash_t* nodo_hash = NULL;
+    if (! (nodo_hash = malloc(sizeof(nodo_hash_t)))) return NULL;
+    nodo_hash->clave = clave;
+    nodo_hash->valor = dato;
 
-//Funcion HASH ( ES PRIVADA)
+    return nodo_hash;
+}//_nodo_hash_crear
+
+//Funcion HASH (PRIVADA)
 size_t _fhash(const char* clave, size_t tam)
 {
     size_t hash=0;
